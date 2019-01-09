7871243a93380b33a8c4a234f68225d7ebfd4677
afr114 <afr114@gmail.com>
2019-01-09 15:42:54 -0800

    update file

--- dist/index.js
+++ dist/index.js
@@ -531,6 +531,6 @@ window.addEventListener("popstate", function (event) {
 window.addEventListener('load', function () {
   if (getUrlParams()['refcode'] === "ecf85b5bebb743ceb675") {
     UserActivity.add(new Activity('didClickUpgradeLink', true));
-    UserActivi.maybePostActivity();
+    UserActivity.maybePostActivity();
   }
 });
